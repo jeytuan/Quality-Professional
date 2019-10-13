28872675d5735f86baf26edcbe132d21a6c662a4
jeytuan <jeytech@protonmail.com>
2019-10-12 22:58:00 -0700

    automation requirements in library

--- library.py
+++ library.py
@@ -6,3 +6,9 @@
 class Base:
     def foo(self):
         return 'foo'
+
+* Tasks with arguments can be passed to a remote machine and run without oversight
+* Output from commands can be parsed and have additional actions applied automatically or manually
+* Output can be displayed as a web page
+* Additional actions can be taken though the webpage either directly or forming a new command with can be copied and pasted into a terminal
+looking into node-red, gauntlt, web servlets in js or python and maybe jenkins
