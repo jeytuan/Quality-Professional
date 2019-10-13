8eb69f90251fc2edefe2b9d72c88b59d588b62e8
root <root@localhost.localdomain>
2019-10-13 00:17:41 -0400

    Added playbook

---
+++ Playbook.py
@@ -0,0 +1,26 @@
+from Cabrillo openlab
+	NDG Ethical Hacking
+	NDG Forensics
+	NISGTC Ethical Hacking
+	NISGTC Forensics
+
+# Catalogue every course lesson and consolidate same attack methods into classes
+
+catalogue NDG Ethical Hacking
+	- Reconnaissance
+	nmap; -sT -F -A -sC 
+	wireshark
+	amap; -A -B
+
+	- Social Engineering Toolkit (SET) Attack
+	setoolkit
+		1) Social-Engineering Attacks
+		2) Website Attack Vectors
+		3) Credential Harvester Attack Method
+		1) Web Templates
+		2) Google
+			default store=/var/www/html
+	# Unusably high latency on "192.168.9.2 sample web template on Co
+
+	- Metasploit Framework Fundamentals
+		
